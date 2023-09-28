7f450f4f936c66aa3d0153b35d9eca5a66e3ec2c
baihaqi <144442840+baihaqi1945@users.noreply.github.com>
2023-09-28 21:26:48 +0700

    Add files via upload

---
+++ belajarPY.py
@@ -0,0 +1,21 @@
+# nama_saya = "Baihaqi Ridho Pahlevi"
+
+# print(nama_saya.find('h'))
+# print(len(nama_saya))
+# print('a' in nama_saya)
+# print(nama_saya.upper())
+# print(nama_saya.count('a'))
+
+
+
+
+
+
+usia = 20
+
+
+if usia > 20:
+    print('yowwman sudah dewasa ')
+
+    else:
+        print('masih muda')
